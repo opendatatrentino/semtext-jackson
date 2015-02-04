563c0bb9aae39bc91d7b13b6a7cde8c356e2af31
David Leoni <davidleonigit-github@yahoo.com>
2015-02-04 14:41:55 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+SemText Jackson 
+===
+<!--
+<p align="center">
+<img alt="Jackan" src="https://github.com/opendatatrentino/jackan/wiki/img/jackan-logo-200px.png" width="150px">
+</p>
+-->
+
+<br/>
+
+Java library for semantic text JSON serialization with Jackson.
+
+**Project status**: developing [milestone 0.5](https://github.com/opendatatrentino/semtext-jackson/issues?milestone=1&state=open) 
+
+**Usage**: see [the wiki](https://github.com/opendatatrentino/semtext-model/wiki)
+
+
+**Features**:
+  * dependency handling with Maven
+  * has a liberal Apache 2.0 license
+  * supports Java 6+
+
+
+**Credits**:
+
+* David Leoni - DISI, University of Trento - david.leoni@unitn.it
+* Ivan Tankoyeu - DISI, University of Trento - tankoyeu@disi.unitn.it
+* Gabor Bella - DISI, University of Trento - Gabor.Bella@unitn.it
+* Simon Anderson - DISI, University of Trento - andersson@disi.unitn.it
+
+
+
+
---
+++ src/test/java/eu/trentorise/opendata/semantics/nlp/test/jackson/JacksonTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright 2015 Trento Rise.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package eu.trentorise.opendata.semantics.nlp.test.jackson;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.guava.GuavaModule;
+import com.google.common.base.Optional;
+import java.io.IOException;
+import java.util.logging.Logger;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * @author David Leoni
+ */
+public class JacksonTest {
+
+    private static final Logger LOG = Logger.getLogger(JacksonTest.class.getName());
+
+    static class A {
+
+        private Optional<String> opt;
+
+        public A() {
+        }
+
+        public Optional<String> getOpt() {
+            return opt;
+        }
+
+        public void setOpt(Optional<String> opt) {
+            this.opt = opt;
+        }
+
+    }
+
+    @Test
+    public void testOptional() throws JsonProcessingException, IOException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new GuavaModule());
+        A a = new A();
+        a.setOpt(Optional.<String>absent());
+        String s = om.writeValueAsString(a);
+        assertTrue(s.contains("null"));
+
+        A ra = om.readValue("{\"opt\":null}", A.class);
+        assertEquals(Optional.absent(), ra.getOpt());
+
+    }
+
+    /**
+     * This fails! As a workaround, we must initialize obj to 'absent' in the
+     * object constructor!
+     *
+     * @throws IOException
+     */
+    @Test
+    @Ignore
+    public void testEmptyOptional() throws IOException {
+        ObjectMapper om = new ObjectMapper();
+        om.registerModule(new GuavaModule());
+
+        A rb = om.readValue("{}", A.class);
+        assertEquals(Optional.absent(), rb.getOpt());
+    }
+}
